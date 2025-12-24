b796d3b9ef354f878ae9d1242093726da6a69115
Alexander <rzn.russia4912@gmail.com>
2025-12-24 22:32:35 +0300

    HW2 is done

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType; // пропиши типизацию
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]; // пропиши типизацию
 };
 
 export const HW2 = () => {
@@ -30,7 +30,7 @@ export const HW2 = () => {
   // ❗ Массив с данными не трогаем!
 
   const users = {
-    myFriends:[
+    myFriends: [
       { id: 1, name: 'John', age: 25, address: { street: '123 Main St', city: 'New York' } },
       { id: 2, name: 'Alice', age: 30, address: { street: '456 Elm St', city: 'San Francisco' } },
       { id: 3, name: 'Bob', age: 35, address: { street: '789 Oak St', city: 'Seattle' } },
@@ -47,7 +47,9 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+    const filteredUsers = currentUsers.myFriends.filter(
+      user => user.address.city === 'Los Angeles'
+    );
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
