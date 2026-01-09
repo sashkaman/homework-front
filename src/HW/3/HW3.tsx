f1393102199058b9a427e26b97d5531cebed9fc4
Alexander <rzn.russia4912@gmail.com>
2026-01-10 00:13:12 +0300

    type handleChange

--- src/HW/3/HW3.tsx
+++ src/HW/3/HW3.tsx
@@ -31,12 +31,12 @@ export const HW3 = () => {
   return (
     <div id={'hw03'}>
       {/*{currentText ? (*/}
-      {/*  <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>*/}
+      <h1 id={'hw03-text'}>ЗДЕСЬ ХОТЕЛОСЬ БЫ УВИДЕТЬ ВВОДИМЫЙ ТЕКСТ</h1>
       {/*) : (*/}
       {/*  <h1 id={'hw03-default-text'}>Здесь появится новое дело</h1> // ничего не меняем, здесь все норм*/}
       {/*)}*/}
 
-      {/*<input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />*/}
+      <input id={'hw03-input'} type="text" value={currentText} onChange={handleChange} />
 
       {/*<button id={'hw03-button'} onClick={() => {}}> // НЕ ХВАТАТЕТ ФУНКЦИИ*/}
       {/*  Сохранить*/}
